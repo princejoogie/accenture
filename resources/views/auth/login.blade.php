985cecf53ad695bab9d4d90d09bd49d52389dcc9
axir <asirrhcaridad@gmail.com>
2019-06-01 22:01:43 +0800

    login.blade.php edited with frontend added new parent named login.blade.php added css for login.blade.php named loginstyle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,76 @@
             </div>
         </div>
     </div>
+</div> -->
+
+<link href="{{ asset('css/loginstyle.css') }}" rel="stylesheet">
+
+<div class="container">
+  <div class="overlay" id="overlay">
+    <div class="sign-in" id="sign-in">
+      <h1>Welcome Back!</h1>
+      <p>To keep connected with us please login with your personal info</p>
+      <button class="switch-button" id="slide-right-button">Sign In</button>
+    </div>
+    <div class="sign-up" id="sign-up">
+      <h1>Hello, Friend!</h1>
+      <p>Enter your personal details and start a journey with us</p>
+      <button class="switch-button" id="slide-left-button">Sign Up</button>
+    </div>
+  </div>
+  <div class="form">
+    <div class="sign-in" id="sign-in-info">
+      <h1>Log in</h1>
+      <form id="sign-in-form" method="POST" action="{{ route('login') }}">  
+          
+        <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+             @error('email')
+                <span class="invalid-feedback" role="alert">
+                     <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            @if (Route::has('password.request'))
+                <br>
+                <a class="forgot-password" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            @endif
+        <button type="submit" class="control-button in">{{ __('Login') }}</button>
+      </form>
+    </div>
+    <div class="sign-up" id="sign-up-info">
+      <h1>{{ __('Register') }}</h1>
+      <form id="sign-up-form" method="POST" action="{{ route('register') }}">
+        @csrf
+        <input id="name" placeholder="Name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+        <button class="control-button up">Sign Up</button>
+      </form>
+    </div>
+  </div>
 </div>
+
 @endsection
